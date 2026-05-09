a5871fa9389563e6a0103105ebb625316241b498
Danish Zia <danishzia0000@outlook.com>
2026-05-09 23:54:25 +0500

    Chapter 1: Exercise 1 and 2 - Done

---
+++ Chapter_1/Exercise1-1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("hello. world\n");
+}
+
+/*
+Compiler will not compile the program if there is any required thing missing like main method parenthesis.
+*/
---
+++ Chapter_1/Exercise1-2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    printf("hello, world\m \n");
+}
+
+/*
+If you try to provide any escape sequence which is not listed compiler will compile the program with warning of unknown escape sequence e.g in case of \m
+Exercise1-2.c:3:25: warning: unknown escape sequence '\m' [-Wunknown-escape-sequence]
+    printf("hello. world\m \n");
+                        ^~
+1 warning generated.
+apple@192 Chapter_1 % ./Exercise1-2.out                   
+
+Output will be like:
+hello, worldm 
+
+*/

06bb70938a794ae5945f5ddf62f8a08e95bd084f
Danish Zia <danishzia0000@outlook.com>
2026-05-10 13:14:47 +0500

    Chapter 1: Exercise 9 and 10 - Done

---
+++ Chapter_1/Exercise1-9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    char c;
+    int in_blank = 0;
+    while((c = getchar()) != EOF){
+        if(c == ' '){
+            if(!in_blank){
+                putchar(c);
+                in_blank = 1;
+            }
+        }
+        else{
+            putchar(c);
+            in_blank = 0;
+        }
+    }
+    return 0;
+}
